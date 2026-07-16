492c7deee4bd415199e4facf959916dfd0a3eaf7
Rusty Pickle <rusty.pickle94@gmail.com>
2026-07-16 19:32:37 +0600

    Fix returning web url instead of server

--- shared/src/config.rs
+++ shared/src/config.rs
@@ -80,7 +80,7 @@ impl Config {
     #[must_use]
     pub fn get_web_ui_url(&self) -> String {
         var("WEB_URL").unwrap_or(
-            self.server_url
+            self.web_ui_url
                 .clone()
                 .unwrap_or(DEFAULT_WEB_URL.to_string()),
         )
